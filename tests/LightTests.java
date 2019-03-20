1e2a711809a38926355522f1095e06a48012038e
nedonator <33222825+nedonator@users.noreply.github.com>
2019-03-20 22:07:38 +0300

    Add files via upload

---
+++ tests/LightTests.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import src.ru.sbt.mipt.oop.*;
+
+import static src.ru.sbt.mipt.oop.SensorEventType.LIGHT_ON;
+import static src.ru.sbt.mipt.oop.SensorEventType.LIGHT_OFF;
+
+public class LightTests {
+    @Test
+    public void test1(){
+        SmartHomeGenerator g = new SimpleSmartHomeGenerator();
+        SmartHome home = g.smartHomeGenerate();
+        EventHandler h = new LightEventHandler();
+        SensorEvent e = new SensorEvent(LIGHT_OFF, "3");
+        for(Room r : home.getRooms()){
+            for(Light d : r.getLights()){
+                Action a = h.handle(e);
+                home.execute(a);
+            }
+        }
+        for(Room r : home.getRooms()){
+            for(Light d : r.getLights()){
+                assert(!d.getId().equals("3") || !d.isOn());
+            }
+        }
+    }
+
+    @Test
+    public void test2(){
+        SmartHomeGenerator g = new SimpleSmartHomeGenerator();
+        SmartHome home = g.smartHomeGenerate();
+        EventHandler h = new LightEventHandler();
+        SensorEvent e = new SensorEvent(LIGHT_ON, "1");
+        Action a = h.handle(e);
+        home.execute(a);
+        for(Room r : home.getRooms()){
+            for(Light d : r.getLights()){
+                assert(!d.getId().equals("1") || d.isOn());
+            }
+        }
+    }
+}
